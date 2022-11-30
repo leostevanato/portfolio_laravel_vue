e8c8591136590e9229319f3456e4608536d193dc
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-11-30 15:59:59 -0300

    Create works, skills and SkillWork migrations

---
+++ database/migrations/2022_11_30_181341_create_works_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+            $table->string('title', 60);
+            $table->string('slug', 60)->unique();
+            $table->text('description')->nullable();
+            $table->string('url', 200)->nullable();
+            $table->string('image', 50)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+};
