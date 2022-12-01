8ce907476d8f7f3ed0601120e9ee2b365ab36a11
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-12-01 15:28:39 -0300

    Create IntetiaLinkButton.vue

---
+++ resources/js/Components/IntetiaLinkButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+import { computed } from '@vue/reactivity';
+
+const props = defineProps(['href', 'style']);
+
+// Default to PrimaryButton style
+const classes = computed(() =>
+    (props.style === "SecondaryButton")
+        ? "inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150"
+        : "inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+);
+</script>
+
+<template>
+    <Link :href="href" :class="classes" as="button" type="button" >
+        <slot />
+    </Link>
+</template>
