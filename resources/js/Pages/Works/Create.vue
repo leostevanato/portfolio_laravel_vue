4067af16650e521f2f065e8df93b1378e4485ec1
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-12-02 11:29:10 -0300

    Update Create.vue

--- resources/js/Pages/Works/Create.vue
+++ resources/js/Pages/Works/Create.vue
@@ -1,11 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/inertia-vue3';
-import CreateWorkForm from './Partials/CreateWorkForm.vue';
-
-defineProps({
-
-});
+import WorkForm from './Partials/WorkForm.vue';
 </script>
 
 <template>
@@ -19,7 +15,7 @@ defineProps({
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <CreateWorkForm
+                    <WorkForm
                         class="max-w-xl"
                     />
                 </div>
