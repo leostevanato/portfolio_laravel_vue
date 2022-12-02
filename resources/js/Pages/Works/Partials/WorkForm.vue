32d3c21c64f1e159e88a2dd0bcf6790c0716fac0
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-12-02 11:28:40 -0300

    Update CreateWorkForm to WorkForm
    - Now the same form component can be used for Create and Update Works

--- resources/js/Pages/Works/Partials/WorkForm.vue
+++ resources/js/Pages/Works/Partials/WorkForm.vue
@@ -7,34 +7,38 @@ import TextArea from '@/Components/TextArea.vue';
 import TextInput from '@/Components/TextInput.vue';
 import slugify from '@/Functions/slugify';
 import { useForm, usePage } from '@inertiajs/inertia-vue3';
-import { computed } from '@vue/reactivity';
-       
-const props = defineProps({
 
+// If not editing then is creating
+const editing = (route().current('works.edit')) ? true : false;
+
+const props = defineProps({
+    work: Object,
+    errors: Object
 });
 
 const user = usePage().props.value.auth.user;
 
-const form = useForm({
-    user_id: user.id,
-    title: '',
-    slug: '',
-    description: null,
-    url: null,
-    image: null
-});
+const form = useForm((editing)
+    ? props.work
+    : {
+        user_id: user.id,
+        title: '',
+        slug: '',
+        description: null,
+        url: null,
+        image: null
+    }
+);
 
 const slugifyTitle = () => {
-    // TODO: implement check if slug exists
+    // TODO: implement check if slug exists add number
     form.slug = slugify(form.title);
 };
 
-const disableSave = computed(() => {
-    return form.title.length > 0 && form.slug.length > 0 ? false : true;
-});
-
 const submit = () => {
-    form.post(route('works.store'));
+    (editing)
+        ? form.patch(route('works.update', form))
+        : form.post(route('works.store'))
 };
 </script>
 
@@ -46,14 +50,7 @@ const submit = () => {
             <div class="!mt-0">
                 <InputLabel for="title" value="Title" />
 
-                <TextInput
-                    id="title"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.title"
-                    required
-                    autofocus
-                />
+                <TextInput id="title" type="text" class="mt-1 block w-full" v-model="form.title" required autofocus />
 
                 <InputError class="mt-2" :message="form.errors.title" />
             </div>
@@ -61,13 +58,7 @@ const submit = () => {
             <div>
                 <InputLabel for="slug" value="Slug" />
 
-                <TextInput
-                    id="slug"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.slug"
-                    required
-                />
+                <TextInput id="slug" type="text" class="mt-1 block w-full" v-model="form.slug" required />
 
                 <SecondaryButton class="mt-2" @click="slugifyTitle">Create slug from title</SecondaryButton>
 
@@ -77,11 +68,7 @@ const submit = () => {
             <div>
                 <InputLabel for="description" value="Description" />
 
-                <TextArea
-                    id="description"
-                    class="mt-1 block w-full"
-                    v-model="form.description"
-                />
+                <TextArea id="description" class="mt-1 block w-full" v-model="form.description" />
 
                 <InputError class="mt-2" :message="form.errors.description" />
             </div>
@@ -89,12 +76,7 @@ const submit = () => {
             <div>
                 <InputLabel for="url" value="URL" />
 
-                <TextInput
-                    id="url"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.url"
-                />
+                <TextInput id="url" type="text" class="mt-1 block w-full" v-model="form.url" />
 
                 <InputError class="mt-2" :message="form.errors.url" />
             </div>
@@ -102,18 +84,13 @@ const submit = () => {
             <div>
                 <InputLabel for="image" value="Image" />
 
-                <TextInput
-                    id="image"
-                    type="text"
-                    class="mt-1 block w-full"
-                    v-model="form.image"
-                />
+                <TextInput id="image" type="text" class="mt-1 block w-full" v-model="form.image" />
 
                 <InputError class="mt-2" :message="form.errors.image" />
             </div>
 
             <div class="flex items-center gap-4">
-                <PrimaryButton :disabled="disableSave || form.processing">Save</PrimaryButton>
+                <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
 
                 <Transition enter-from-class="opacity-0" leave-to-class="opacity-0" class="transition ease-in-out">
                     <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
