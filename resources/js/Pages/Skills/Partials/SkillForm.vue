9d4b7d6a3db1e25c49c9b973fb23eb2b4efdce8b
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-12-05 09:42:21 -0300

    Create Skill create/edit form

---
+++ resources/js/Pages/Skills/Partials/SkillForm.vue
@@ -0,0 +1,50 @@
+<script setup>
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
+import { useForm } from '@inertiajs/inertia-vue3';
+
+// If not editing then is creating
+const editing = (route().current('skills.edit')) ? true : false;
+
+const props = defineProps({
+    skill: Object,
+    errors: Object
+});
+
+const form = useForm((editing)
+    ? props.skill
+    : {
+        title: ''
+    }
+);
+
+const submit = () => {
+    (editing)
+        ? form.patch(route('skills.update', form))
+        : form.post(route('skills.store'))
+};
+</script>
+
+<template>
+    <section>
+        <form @submit.prevent="submit" class="space-y-6">
+            <div class="!mt-0">
+                <InputLabel for="title" value="Title" />
+
+                <TextInput id="title" type="text" class="mt-1 block w-full" v-model="form.title" required autofocus />
+
+                <InputError class="mt-2" :message="form.errors.title" />
+            </div>
+
+            <div class="flex items-center gap-4">
+                <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
+
+                <Transition enter-from-class="opacity-0" leave-to-class="opacity-0" class="transition ease-in-out">
+                    <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
+                </Transition>
+            </div>
+        </form>
+    </section>
+</template>
