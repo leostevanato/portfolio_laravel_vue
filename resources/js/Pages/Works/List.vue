c9e528bc8383e9fa9dafab5fe43ef3d9e66a45fb
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-12-06 12:06:14 -0300

    Update Works List to handle the image

--- resources/js/Pages/Works/List.vue
+++ resources/js/Pages/Works/List.vue
@@ -1,10 +1,15 @@
 <script setup>
-import GuestLayout from '@/Layouts/GuestLayout.vue';
 import { Head } from '@inertiajs/inertia-vue3';
 
 defineProps({
     works: Array,
 });
+
+function workImage(img) {
+    return !img
+        ? '<div class="flex"><svg class="m-auto fill-gray-400" width="32px" height="32px" viewBox="0 0 32 32" id="icon" xmlns="http://www.w3.org/2000/svg"><defs><style>.cls-1{fill:none;}</style></defs><title>no-image</title><path d="M30,3.4141,28.5859,2,2,28.5859,3.4141,30l2-2H26a2.0027,2.0027,0,0,0,2-2V5.4141ZM26,26H7.4141l7.7929-7.793,2.3788,2.3787a2,2,0,0,0,2.8284,0L22,19l4,3.9973Zm0-5.8318-2.5858-2.5859a2,2,0,0,0-2.8284,0L19,19.1682l-2.377-2.3771L26,7.4141Z"/><path d="M6,22V19l5-4.9966,1.3733,1.3733,1.4159-1.416-1.375-1.375a2,2,0,0,0-2.8284,0L6,16.1716V6H22V4H6A2.002,2.002,0,0,0,4,6V22Z"/><rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width="32" height="32"/></svg></div>'
+        : '<img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="" class="h-full w-full object-cover object-center group-hover:opacity-75">';
+}
 </script>
 
 <template>
@@ -27,10 +32,8 @@ defineProps({
                             class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                             <a v-for="work in works" :key="work.id" href="#" class="group">
                                 <div
-                                    class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-                                    <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg"
-                                        alt=""
-                                        class="h-full w-full object-cover object-center group-hover:opacity-75">
+                                    class="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8"
+                                    v-html="workImage(work.image)">
                                 </div>
                                 <h3 class="mt-4 text-md text-gray-700 font-bold">{{ work.title }}</h3>
                                 <p v-if="work.url" class="mt-1 text-md text-gray-700">{{ work.url }}</p>
