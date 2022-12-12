dc2272c84788a03f290336721199d3732327a30e
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-12-12 13:26:37 -0300

    Make portfolio be the welcome page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\WorkController;
+use App\Http\Controllers\PortfolioController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -17,14 +17,7 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get('/', [PortfolioController::class, 'list'])->name('portfolio');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -35,7 +28,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::get('/works/list', [WorkController::class, 'list'])->name('works-list');
 
 require __DIR__.'/auth.php';
