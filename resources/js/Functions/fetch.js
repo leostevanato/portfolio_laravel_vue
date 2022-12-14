bff2f4289e3295f687a198ff721d3ca03c0b50e8
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-12-14 10:00:02 -0300

    Add loading true or false to fetch return

--- resources/js/Functions/fetch.js
+++ resources/js/Functions/fetch.js
@@ -1,13 +1,20 @@
 import { ref } from 'vue';
 
 export function useFetch(url) {
+	const loading = ref(true);
 	const data = ref(null);
 	const error = ref(null);
 
 	fetch(url)
-		.then((res) => res.json())
-		.then((json) => (data.value = json))
-		.catch((err) => (error.value = err));
+		.then(res => res.json())
+		.then(json => {
+			loading.value = false;
+			data.value = json;
+		})
+		.catch(err => {
+			loading.value = false;
+			error.value = err;
+		});
 
-	return { data, error };
+	return { loading, data, error };
 }
