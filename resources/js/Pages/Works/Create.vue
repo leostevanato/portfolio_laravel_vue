68fcb7c14ea5a0a4921dd56a83c0bd910c2795b1
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-12-28 14:29:16 -0300

    Update to work with portfolios and some layout

--- resources/js/Pages/Works/Create.vue
+++ resources/js/Pages/Works/Create.vue
@@ -4,12 +4,13 @@ import { Head } from '@inertiajs/inertia-vue3';
 import WorkForm from './Partials/WorkForm.vue';
 
 const props = defineProps({
+    portfolios: Object,
     skills: Object
 });
 </script>
 
 <template>
-    <Head title="Work" />
+    <Head title="New work" />
 
     <AuthenticatedLayout>
         <template #header>
@@ -17,10 +18,11 @@ const props = defineProps({
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="max-w-fit xl:max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                     <WorkForm
-                        class="max-w-xl"
+                        class="max-w-fit"
+                        :portfolios="portfolios"
                         :skills="skills"
                     />
                 </div>
