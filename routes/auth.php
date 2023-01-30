7ffbac7f369b629ecda77d10af099d81af0ba22b
leostevanato <47180899+leostevanato@users.noreply.github.com>
2023-01-30 12:23:17 -0300

    Add use SettingController to auth route

--- routes/auth.php
+++ routes/auth.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\PortfolioController;
+use App\Http\Controllers\SettingController;
 use App\Http\Controllers\SkillController;
 use App\Http\Controllers\WorkController;
 use Illuminate\Support\Facades\Route;
